0b3a44a5ee42aad2f89aa605204d2de66a79f17e
NandawulaJoan <jnandawula77@gmail.com>
2025-04-25 03:47:08 +0300

    still working on frontend

---
+++ Desktop/ozzy/ozzy.py
@@ -0,0 +1,11 @@
+def add1(num1,num2):
+    sum = num1 + num2
+    print(sum)
+    return sum
+
+
+def sub1(num1,num2):
+    sub = num1 - num2
+    print(sub)
+    return sub
+
---
+++ Desktop/ozzy/run.py
@@ -0,0 +1,4 @@
+import ozzy
+print("this is the result of the function from te module",ozzy.add1(10,20))
+
+ozzy.add1(10,30)
